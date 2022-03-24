fefc0e7b0e03e08dd19750c850d20cff9d0e5cbf
chongstephen <72903217+chongstephen@users.noreply.github.com>
2022-03-23 21:52:06 -0400

    Added overlay animation for ColorBox
    Added style to display overlay animation for each color box.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,11 +3,30 @@ import "./ColorBox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { copied: false };
+    this.changeCopyState = this.changeCopyState.bind(this);
+  }
+  changeCopyState() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
   render() {
     const { name, background } = this.props;
+    const { copied } = this.state;
     return (
-      <CopyToClipboard text={background}>
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background }} className="ColorBox">
+          <div
+            style={{ background }}
+            className={`copy-overlay ${copied && "show"}`}
+          />
+          <div className={`copy-msg ${copied && "show"}`}>
+            <h1>copied!</h1>
+            <p>{this.props.background}</p>
+          </div>
           <div className="copy-container">
             <div className="box-content">
               <span>{name}</span>
@@ -22,3 +41,4 @@ class ColorBox extends Component {
 }
 
 export default ColorBox;
+
