b9888f023933c719c8b461be493ce478aacb8b38
chongstephen <72903217+chongstephen@users.noreply.github.com>
2022-03-27 17:41:20 -0400

    Added Footer
    Added palette footer and fixed some styling with palette view height

--- src/Palette.js
+++ src/Palette.js
@@ -17,10 +17,10 @@ class Palette extends Component {
     this.setState({format: val});
   }
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map((color) => (
-      <ColorBox background={color[format]} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} key={color.id} />
     ));
     return (
       <div className="Palette">
@@ -30,9 +30,12 @@ class Palette extends Component {
           handleChange={this.changeFormat}
         />
 
-        {/* Navbar goes here */}
+   
         <div className="Palette-colors">{colorBoxes}</div>
-        {/* footer */}
+        <footer className="Palette-footer">
+        {paletteName}
+        <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     );
   }
