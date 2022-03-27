eb18e78b8030dc877a1d79f4bc9b1d65183ebd88
chongstephen <72903217+chongstephen@users.noreply.github.com>
2022-03-27 15:27:24 -0400

    Add Select and MaterialUI
    added Select hamb menu. Installed Material UI and fixed a bug dealing with rgba not displaying color properly.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,29 +1,47 @@
 import React, { Component } from "react";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
-import './Navbar.css';
+import "./Navbar.css";
 
 class Navbar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {format: "hex"};
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(e){
+        this.setState({format: e.target.value});
+        this.props.handleChange(e.target.value);
+    }
   render() {
-      const {level, changeLevel} = this.props;
+    const { level, changeLevel} = this.props;
+    const {format} = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
           <a href="#">ReactColorPicker</a>
         </div>
         <div className="slider-container">
-            <span>Level: {level}</span>
-        <div className="slider">
-          <Slider
-            defaultValue={level}
-            min={100}
-            max={900}
-            step={100}
-            onAfterChange={changeLevel}
-          />
+          <span>Level: {level}</span>
+          <div className="slider">
+            <Slider
+              defaultValue={level}
+              min={100}
+              max={900}
+              step={100}
+              onAfterChange={changeLevel}
+            />
+          </div>
         </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">Hex - #fffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgb(255,255,255,1.0)</MenuItem>
+          </Select>
         </div>
-   
       </header>
     );
   }
--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -7,22 +7,25 @@ function generatePalette(starterPalette) {
     paletteName: starterPalette.paletteName,
     id: starterPalette.id,
     emoji: starterPalette.emoji,
-    colors: {}
+    colors: {},
   };
   for (let level of levels) {
     newPalette.colors[level] = [];
   }
   for (let color of starterPalette.colors) {
-      let scale = getScale(color.color, 10).reverse();
-      for(let i in scale){
-          newPalette.colors[levels[i]].push({
-              name: `${color.name} ${levels[i]}`,
-              id: color.name.toLowerCase().replace(/ /g, "-"),
-              hex: scale[i],
-              rgb: chroma(scale[i]).css(),
-              rgba: chroma(scale[i]).css().replace("rgba", "rgba").replace(")", "1.0")
+    let scale = getScale(color.color, 10).reverse();
+    for (let i in scale) {
+      newPalette.colors[levels[i]].push({
+        name: `${color.name} ${levels[i]}`,
+        id: color.name.toLowerCase().replace(/ /g, "-"),
+        hex: scale[i],
+        rgb: chroma(scale[i]).css(),
+        rgba: chroma(scale[i])
+          .css()
+          .replace("rgb", "rgba")
+          .replace(")", ",1.0"),
       });
-  }
+    }
   }
   return newPalette;
 }
@@ -32,11 +35,7 @@ function getRange(hexColor) {
 }
 
 function getScale(hexColor, numberOfColors) {
-  return chroma
-  .scale(getRange(hexColor))
-  .mode("lab")
-  .colors(numberOfColors);
+  return chroma.scale(getRange(hexColor)).mode("lab").colors(numberOfColors);
 }
 
-
-export {generatePalette};
+export { generatePalette };
