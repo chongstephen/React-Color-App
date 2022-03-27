8baa5aee8afab569b3434869f69538dfb27b994c
chongstephen <72903217+chongstephen@users.noreply.github.com>
2022-03-27 14:45:11 -0400

    Styled Slider
    Added style to the slider bar

--- src/Palette.js
+++ src/Palette.js
@@ -1,36 +1,43 @@
-import React, {Component} from 'react';
-import ColorBox from './ColorBox';
-import "./Palette.css"
-import Slider, {Range} from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "rc-slider/assets/index.css";
+import "./Palette.css";
+import Slider from "rc-slider";
 
 
 class Palette extends Component {
-    constructor(props){
-        super(props);
-        this.state = {level: 500}
-        this.changeLevel = this.changeLevel.bind(this);
-    }
-    changeLevel(level){
-        this.setState({ level});
-    }
-    render(){
-        const {colors} =  this.props.palette;
-        const {level} =  this.state;
-        const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color.hex} name={color.name} />
-        ));
-        return(
-            <div className="Palette">
-                <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={this.changeLevel}/>
-                {/* Navbar goes here */}
-                <div className="Palette-colors">
-                    {colorBoxes}
-                </div>
-                {/* footer */}
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+  changeLevel(level) {
+    this.setState({ level });
+  }
+  render() {
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox background={color.hex} name={color.name} />
+    ));
+    return (
+      <div className="Palette">
+        <div className="slider">
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+          />
+        </div>
+
+        {/* Navbar goes here */}
+        <div className="Palette-colors">{colorBoxes}</div>
+        {/* footer */}
+      </div>
+    );
+  }
 }
 
 export default Palette;
