ec0e228f9c10c676ebc104d0a387f67b11f3c88d
chongstephen <72903217+chongstephen@users.noreply.github.com>
2022-03-30 21:51:49 -0400

    Updated Palette Route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, {Component} from 'react';
-import {Route, Switch} from 'react-router-dom';
-import Palette from './Palette';
-import seedColors from './seedColors';
-import {generatePalette} from './colorHelpers';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
       <Switch>
-  <Route exact path="/" render={() => <h1>Palette List Goes Here</h1>}/>
-      <Route exact path="/palette/:id" render={() => <h1>Individual Palette</h1>}/>
+        <Route exact path="/" render={() => <h1>Palette List Goes Here</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
       </Switch>
-    
+
       // <div>
       // <Palette palette={generatePalette(seedColors[4])} />
       // </div>
-    )
+    );
   }
 }
 
