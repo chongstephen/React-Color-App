f9f5aea0164a15f393764dddb9a8886428829f6b
chongstephen <72903217+chongstephen@users.noreply.github.com>
2022-03-30 22:08:53 -0400

    Added PaletteList
    Added PaletteList and Links

---
+++ src/PaletteList.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class PaletteList extends Component {
+    render(){
+        const { palette } = this.props;
+        return(
+            <div>
+            <h1>Palette List</h1>
+            {palette.map(palette => (
+                <p>
+                <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+                </p>
+            ))}
+            </div>
+        );
+    }
+}
+
+export default PaletteList;
