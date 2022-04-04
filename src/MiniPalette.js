5c416500f39783ee0d9f4be809df5b205f1aa198
chongstephen <72903217+chongstephen@users.noreply.github.com>
2022-04-03 21:08:30 -0400

    Finished styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,22 +2,25 @@ import React from "react";
 import { withStyles } from "@material-ui/styles";
 
 const styles = {
-  root:{
+  root: {
     backgroundColor: "white",
     border: "1px solid black",
     borderRadius: "5px",
     padding: "0.5rem",
     position: "relative",
     overflow: "hidden",
-    "&:hover ":{
-        cursor: "pointer"
-    }
+    "&:hover ": {
+      cursor: "pointer",
+    },
   },
-  colors:{
-    backgroundColor: "grey",
-
+  colors: {
+    backgroundColor: "#dae1e4",
+    height: "150px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden"
   },
-  title:{
+  title: {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
@@ -25,20 +28,38 @@ const styles = {
     color: "black",
     paddingTop: "0.5rem",
     fontSize: "1rem",
-    position: "relative"
+    position: "relative",
   },
-  emoji:{
+  emoji: {
     marginLeft: "0.5rem",
-    fontSize: "1.5rem"
-  }
+    fontSize: "1.5rem",
+  },
+  miniColor: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBotton: "-3.5px",
+  },
 };
 
 function MiniPalette(props) {
-  const { classes, paletteName, emoji } = props;
+  const { classes, paletteName, emoji, colors } = props;
+  const miniColorBoxes = colors.map((color) => (
+    <div
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
+      key={color.name}
+    />
+  ));
+
   return (
     <div className={classes.root}>
-      <div className={classes.colors}></div>
-      <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
+      <div className={classes.colors}>{miniColorBoxes}</div>
+      <h5 className={classes.title}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
+      </h5>
     </div>
   );
 }
