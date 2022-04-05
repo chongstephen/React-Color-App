3ddf72ba48618304a44a85f768fd56705f13f9b8
chongstephen <72903217+chongstephen@users.noreply.github.com>
2022-04-04 21:24:34 -0400

    Added Link to ColorBox and Added Route

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
             />
           )}
         />
+        <Route path="/palette/:paletteId/:colorId" render={() => <h1>Single Color Page</h1>}/>
       </Switch>
     );
   }
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./ColorBox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import {Link} from 'react-router-dom';
 
 class ColorBox extends Component {
   constructor(props) {
@@ -33,7 +34,10 @@ class ColorBox extends Component {
             </div>
             <button className="copy-button">Copy</button>
           </div>
+          <Link to='/' onClick={e => e.stopPropagation()}>
           <span className="see-more">More</span>
+          </Link>
+        
         </div>
       </CopyToClipboard>
     );
