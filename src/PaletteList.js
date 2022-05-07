9f0f1fb8030f9c9fd12e9dd97a622c4314fb5304
chongstephen <72903217+chongstephen@users.noreply.github.com>
2022-05-07 15:58:30 -0400

    Added SVG background

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -15,7 +15,7 @@ class PaletteList extends Component {
       <div className={classes.root} onClick={this.props.handleClick}>
         <div className={classes.container}>
           <nav className={classes.nav}>
-            <h1>Colors</h1>
+            <h1 className={classes.heading}>Colors</h1>
             <Link to="/palette/new">Create New Palette</Link>
           </nav>
           <div className={classes.palettes}>
