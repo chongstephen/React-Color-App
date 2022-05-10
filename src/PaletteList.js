d54939a4d9377816116bee32a055080f9abec6c2
chongstephen <72903217+chongstephen@users.noreply.github.com>
2022-05-09 21:45:05 -0400

    Refactored with PureComponent

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -26,6 +26,7 @@ class PaletteList extends Component {
     this.openDialog = this.openDialog.bind(this);
     this.closeDialog = this.closeDialog.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
+    this.goToPalette = this.goToPalette.bind(this);
   }
   openDialog(id){
     this.setState({openDeleteDialog: true, deletingId: id})
@@ -41,7 +42,7 @@ class PaletteList extends Component {
     this.closeDialog();
   }
   render() {
-    const { palettes, classes, deletePalette, deletingId } = this.props;
+    const { palettes, classes } = this.props;
     const {openDeleteDialog} = this.state;
     return (
       <div className={classes.root} onClick={this.props.handleClick}>
@@ -56,7 +57,7 @@ class PaletteList extends Component {
               <CSSTransition key={palette.id} classNames="fade" timeout={500}>
                 <MiniPalette
                   {...palette}
-                  handleClick={() => this.goToPalette(palette.id)}
+                  goToPalette={this.goToPalette}
                   // handleDelete={deletePalette}
                   openDialog = {this.openDialog}
                   key={palette.id}
