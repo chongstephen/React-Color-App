29539fa27b19fb114a94a11732d8d799b63b6df7
chongstephen <72903217+chongstephen@users.noreply.github.com>
2022-05-09 21:53:30 -0400

    Fixed navbar slider size and added classnames

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
-import styles from './styles/ColorBoxStyles';
+import styles from "./styles/ColorBoxStyles";
+import classNames from "classnames";
 import { withStyles } from "@material-ui/styles";
-
-
 
 class ColorBox extends Component {
   constructor(props) {
@@ -18,7 +17,8 @@ class ColorBox extends Component {
     });
   }
   render() {
-    const { name, background, moreUrl, showingFullPalette, classes } = this.props;
+    const { name, background, moreUrl, showingFullPalette, classes } =
+      this.props;
     const { copied } = this.state;
 
     return (
@@ -26,9 +26,14 @@ class ColorBox extends Component {
         <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`${classes.copyOverlay} ${copied && classes.showOverlay}`}
+            className={classNames(classes.copyOverlay, {
+              [classes.showOverlay]: copied,
+            })}
           />
-          <div className={`${classes.copyMsg} ${copied && classes.showMsg}`}>
+
+          <div className={classNames(classes.copyMsg, {
+              [classes.showMsg]: copied,
+            })}>
             <h1>copied!</h1>
             <p className={classes.copyText}>{this.props.background}</p>
           </div>
@@ -36,15 +41,11 @@ class ColorBox extends Component {
             <div className={classes.boxContent}>
               <span className={classes.colorName}>{name}</span>
             </div>
-            <button className={classes.copyButton}>
-              Copy
-            </button>
+            <button className={classes.copyButton}>Copy</button>
           </div>
           {showingFullPalette && (
             <Link to={moreUrl} onClick={(e) => e.stopPropagation()}>
-              <span className={classes.seeMore}>
-                MORE
-              </span>
+              <span className={classes.seeMore}>MORE</span>
             </Link>
           )}
         </div>
